b0bc670fdf3d8f4db28346f49243008e3be8efa1
Debrena McEwen <debrenamcewen@gmail.com>
2017-09-25 14:25:00 -0700

    Add error handling to tasks.js

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const pgp = pgPromise()
 const db = pgp(`postgres://${process.env.USER}@localhost:5432/todosql`)
 
 const add = 'INSERT INTO tasks (task) VALUES ($1) RETURNING id'
-const deleteTask = 'DELETE FROM tasks WHERE id = $1'
+const deleteTask = 'DELETE FROM tasks WHERE id = $1 RETURNING *'
 const list = 'SELECT id, task FROM tasks'
 
 exports.addTask = function(task) {
@@ -15,7 +15,12 @@ exports.listTasks = function() {
 }
 
 exports.deleteTask = function(task) {
-    return db.none(deleteTask, task)
+    return db.one(deleteTask, task)
 }
 
 exports.pgp = pgp
+
+//.catch(err => {
+// console.log(err)
+// return Promise.reject(err)  
+//     })
